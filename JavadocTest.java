ea36d04721f888dd226112e7c58dd99f74dd633c
Peng Li @BWG <lipengweb@gmail.com>
2017-06-10 08:44:19 -0400

    Add javadoc example

---
+++ JavadocTest.java
@@ -0,0 +1,24 @@
+package peng;
+/**
+ * Description:
+ * <br>Copyright
+ * <br>This program is protected by Copyright laws.
+ * @author Peng Li
+ * @version 1.0
+ */
+
+// To build this doc, we need to call javadoc
+// For example, javadoc -d ./doc JavadocTest.java
+public class JavadocTest {
+	/**
+	 * Simple Test for variable
+	 */
+	protected String name;
+	/**
+	 * Main method
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
